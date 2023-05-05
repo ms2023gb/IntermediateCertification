4fc85d6f29688d4d5538d8c7b6bed5de41c75951
Stanislav <ms2023gb@gmail.com>
2023-05-05 23:40:52 +0300

    my solution

--- Program.cs
+++ Program.cs
@@ -12,3 +12,55 @@
 
     ["Russia", "Denmark", "Kazan"] → []
 */
+
+Console.Clear();
+const int len = 3;
+
+string[] array = new string[] { "Hello", "2", "world", ":-)" };
+PrintArray(array);
+PrintArray(FindInArray(array));
+System.Console.WriteLine();
+
+array = new string[] { "1234", "1567", "-2", "computer science" };
+PrintArray(array);
+PrintArray(FindInArray(array));
+System.Console.WriteLine();
+
+array = new string[] { "Russia", "Denmark", "Kazan" };
+PrintArray(array);
+PrintArray(FindInArray(array));
+
+/* 
+    Метод создание нового массива за два цикла перебора
+    Первый раз считаем количество нужных элементов
+    Создаем массив нужного размера и вторым циклом заполняем новый массив
+
+*/
+
+string[] FindInArray(string[] arrayForFind)
+{
+    int count = 0;
+    for (int i = 0; i < arrayForFind.Length; i++)
+    {
+        if (arrayForFind[i].Length <= len)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int index = 0;
+    for (int j = 0; j < arrayForFind.Length; j++)
+    {
+        if (arrayForFind[j].Length <= len)
+        {
+            newArray[index] = arrayForFind[j];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] arrayForPrint)
+{
+    Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
