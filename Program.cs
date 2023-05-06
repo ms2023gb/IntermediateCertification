24a61760eeefae9a62ee3d9eb7f3f3e4f7c19126
Stanislav <ms2023gb@gmail.com>
2023-05-06 22:15:05 +0300

    update variables

--- Program.cs
+++ Program.cs
@@ -14,33 +14,34 @@
 */
 
 Console.Clear();
-const int len = 3; // константа для длины строк меньше 3
+// переменные для длины строк больше или равно 1 и меньше или равно 3
+int minStringLength = 1;
+int maxStringLength = 3;
 
 string[] array = new string[] { "Hello", "2", "world", ":-)" };
 PrintArray(array);
-PrintArray(FindInArray(array));
+PrintArray(FindInArray(array, minStringLength, maxStringLength));
 System.Console.WriteLine();
 
 array = new string[] { "1234", "1567", "-2", "computer science" };
 PrintArray(array);
-PrintArray(FindInArray(array));
+PrintArray(FindInArray(array, minStringLength, maxStringLength));
 System.Console.WriteLine();
 
 array = new string[] { "Russia", "Denmark", "Kazan" };
 PrintArray(array);
-PrintArray(FindInArray(array));
+PrintArray(FindInArray(array, minStringLength, maxStringLength));
 
 /*
-    Метод создания нового массива за один проход,
-    используя добавление нового элемента в массив
+    Метод создания нового массива за один проход, путем добавление нового элемента в массив
 */
 
-string[] FindInArray(string[] arrayForFind)
+string[] FindInArray(string[] arrayForFind, int minStringLengthForFind, int maxStringLengthForFind)
 {
     string[] newArray = new string[0];
     for (int i = 0; i < arrayForFind.Length; i++)
     {
-        if (arrayForFind[i].Length <= len)
+        if (arrayForFind[i].Length >= minStringLengthForFind && arrayForFind[i].Length <= maxStringLengthForFind)
         {
             newArray = newArray.Append(arrayForFind[i]).ToArray();
         }
